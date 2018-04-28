830ca0ca9464a24d44d9f06a50d40aec902f9cba
rose <lwcrose@126.com>
2018-04-28 17:47:36 +0800

    update

--- app.js
+++ app.js
@@ -10,3 +10,30 @@ var app2 = new Vue({
         message: '页面加载于 ' + new Date().toLocaleString()
     }
 })
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: '学习 JavaScript' },
+            { text: '学习 Vue' },
+            { text: '整个牛项目' }
+        ]
+    }
+})
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
