f62270840cb95b4139cb88201cf2dbfb319cd5a0
rose <lwcrose@126.com>
2018-04-28 15:00:51 +0800

    try vuejs

---
+++ app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: '页面加载于 ' + new Date().toLocaleString()
+    }
+})
